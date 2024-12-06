60234ae81503fb87472851e880aadc28caf4c7b2
Firas Jebari <Contact@firas-jebari.tn>
2024-12-07 00:54:50 +0100

    config imroved

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   // output: 'export', // Enable static export
-
+  trailingSlash: true, // Adds a trailing slash to all paths
 };
 
 export default nextConfig;
