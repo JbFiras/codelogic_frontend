b493f1e16da8011123b7a0182cffe22a63b91e86
Firas Jebari <Contact@firas-jebari.tn>
2024-12-07 02:01:35 +0100

    favicon added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export', // Enable static export
+  output: 'export', // Enable static export
   trailingSlash: true, // Adds a trailing slash to all paths
+  images: {
+    domains: [''], // Add allowed domains here
+  },
 };
 
 export default nextConfig;
